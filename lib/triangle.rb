433aa95045c3d36c1a14ff1694c433015e4798c2
FrankieMFerreira <FrankieMFerreira@gmail.com>
2016-12-07 20:25:37 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,29 +1,41 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :side1, :side2, :side3, :triangle_sides
 
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
+    @triangle_sides = []
+    @triangle_sides << side1
+    @triangle_sides << side2
+    @triangle_sides << side3
   end
 
   def kind
-    if @side1 == 0 && @side2 == 0 && @side3 == 0
-      raise TriangleError.new
-    elsif @side1 < 0 || @side2 < 0 || @side3 < 0
-      raise TriangleError.new
-    elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
-      raise TriangleError.new
-    elsif @side1 == @side2 && @side2 == @side3 && @side3 == @side1
-      return :equilateral
-    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
-      return :isosceles
-    elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
-      return :scalene
+    if valid?
+      if @triangle_sides.uniq.count == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.count == 2
+        return :isosceles
+      elsif @triangle_sides.uniq.count == 3
+        return :scalene
+      end
     end
   end
 
 end
+
+  def valid?
+    if @triangle_sides.any? {|side| side <= 0}
+      false
+      raise TriangleError.new
+    elsif @triangle_sides.sort[0]+ @triangle_sides.sort[1] <= @triangle_sides.sort[2]
+      false
+      raise TriangleError.new
+    else
+      true
+    end
+  end
 
 
   class TriangleError < StandardError
